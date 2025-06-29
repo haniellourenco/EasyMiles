a738b01cf4421180f7a2ca90ca97bf5ae7c298f8
Haniel <hanielohn2016@gmail.com>
2025-06-29 01:31:46 -0300

    feat: login page
    feito a tela de login e adicionado avatar/nome no header da aplicação para fazer logout

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,11 +1,26 @@
 import { Routes } from '@angular/router';
+import { LoginComponent } from './pages/auth/login/login.component';
+import { MainLayoutComponent } from './layouts/main-layout/main-layout.component';
 
 export const routes: Routes = [
-  { path: '', pathMatch: 'full', redirectTo: '' },
   {
-    path: 'wallets',
-
-    loadChildren: () =>
-      import('./pages/wallets/wallets.routes').then((m) => m.WALLETS_ROUTES),
+    path: 'login',
+    component: LoginComponent,
   },
+  {
+    path: '',
+    component: MainLayoutComponent,
+    children: [
+      { path: '', pathMatch: 'full', redirectTo: 'wallets' },
+      {
+        path: 'wallets',
+        loadChildren: () =>
+          import('./pages/wallets/wallets.routes').then(
+            (m) => m.WALLETS_ROUTES
+          ),
+      },
+    ],
+  },
+  // Redireciona qualquer rota desconhecida para o login
+  { path: '**', redirectTo: 'login' },
 ];
---
+++ frontend/src/app/services/auth.service.ts
@@ -0,0 +1,69 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable, BehaviorSubject, tap, switchMap, of } from 'rxjs';
+import { Router } from '@angular/router';
+
+export interface AuthResponse {
+  refresh: string;
+  access: string;
+}
+
+// resposta de /api/users/me/
+export interface UserProfile {
+  id: number;
+  username: string;
+  email: string;
+  first_name: string;
+  last_name: string;
+  cpf: string;
+}
+
+@Injectable({
+  providedIn: 'root',
+})
+export class AuthService {
+  private apiUrl = 'http://127.0.0.1:8000/api';
+  private readonly TOKEN_KEY = 'auth_token';
+
+  private currentUserSubject = new BehaviorSubject<UserProfile | null>(null);
+  public currentUser$ = this.currentUserSubject.asObservable();
+
+  constructor(private http: HttpClient, private router: Router) {}
+
+  login(credentials: any): Observable<UserProfile> {
+    return this.http
+      .post<AuthResponse>(`${this.apiUrl}/auth/token/`, credentials)
+      .pipe(
+        tap((response) => this.saveToken(response.access)),
+        switchMap(() => this.fetchAndStoreUserProfile())
+      );
+  }
+
+  fetchAndStoreUserProfile(): Observable<UserProfile> {
+    if (!this.getToken()) {
+      return of({} as UserProfile);
+    }
+
+    return this.http
+      .get<UserProfile>(`${this.apiUrl}/users/me/`)
+      .pipe(tap((user) => this.currentUserSubject.next(user)));
+  }
+
+  saveToken(token: string): void {
+    localStorage.setItem(this.TOKEN_KEY, token);
+  }
+
+  getToken(): string | null {
+    return localStorage.getItem(this.TOKEN_KEY);
+  }
+
+  isLoggedIn(): boolean {
+    return !!this.getToken();
+  }
+
+  logout(): void {
+    localStorage.removeItem(this.TOKEN_KEY);
+    this.currentUserSubject.next(null);
+    this.router.navigate(['/login']);
+  }
+}
