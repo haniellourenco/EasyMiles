6504440db38c1317c94d8c52b5b6b93d30ff8f23
Haniel <hanielohn2016@gmail.com>
2025-11-24 00:14:46 -0300

    fix custom-rate table change

--- backend/api/models.py
+++ backend/api/models.py
@@ -16,9 +16,14 @@ class LoyaltyProgram(models.Model):
     )
     name = models.CharField(max_length=100, unique=True)
     currency_type = models.IntegerField(choices=CURRENCY_TYPE_CHOICES)
+    custom_rate = models.DecimalField(
+        max_digits=10, decimal_places=2, default=0.00,
+        help_text="Valor de mercado estimado por 1.000 milhas/pontos"
+    ) 
     is_active = models.BooleanField(default=True)
     is_user_created = models.BooleanField(default=False)
     created_at = models.DateTimeField(auto_now_add=True)
+    
 
     def __str__(self):
         return self.name
@@ -57,10 +62,7 @@ class LoyaltyAccount(models.Model):
         max_digits=12, decimal_places=2, null=True, blank=True,
         help_text="Custo médio por milheiro"
     )
-    custom_rate = models.DecimalField(
-        max_digits=10, decimal_places=4, null=True, blank=True,
-        help_text="Cotação personalizada por milheiro para avaliação de valor"
-    ) 
+    
     last_updated = models.DateTimeField(
         help_text="Data da última atualização de saldo/informações desta conta no programa de fidelidade"
     ) 
